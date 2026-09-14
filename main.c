c93afdc69e0784bc7c7c5eae11a671b4afff5926
SalzDevs <a49000@alunos.isel.pt>
2026-09-14 11:13:35 +0100

    token struct and token type enum

--- main.c
+++ main.c
@@ -1,5 +1,33 @@
 #include "stdio.h" 
+#include "string.h"
 
+typedef enum {
+  TOK_FUNC,
+  TOK_INT,
+  TOK_IF,
+  TOK_FOR,
+  TOK_BREAK,
+  TOK_RETURN,
+  TOK_NAME,
+  TOK_NUMBER,
+  TOK_ASSIGN, 
+  TOK_EQ,   
+  TOK_PLUS, 
+  TOK_GT,
+  TOK_LPAREN,
+  TOK_RPAREN,
+  TOK_LBRACE,
+  TOK_RBRACE,
+  TOK_SEMI,
+  TOK_COMMA
+} TokenType;
+
+typedef struct {
+  TokenType type;
+  char* text;
+  int col;
+  int row;
+} Token ;
 
 int main(){
   printf("Hello world!\n"); 
