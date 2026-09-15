f065a29137cf7f7c48ac920b435ed8aa1f8ee00c
SalzDevs <a49000@alunos.isel.pt>
2026-09-15 10:42:04 +0100

    add v0 lexer with next_token and main test loop

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include "stdio.h" 
 #include "string.h"
+#include "assert.h"
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
 
 typedef enum {
   TOK_FUNC,
@@ -19,16 +23,109 @@ typedef enum {
   TOK_LBRACE,
   TOK_RBRACE,
   TOK_SEMI,
-  TOK_COMMA
+  TOK_COMMA,
+  TOK_EOF
 } TokenType;
 
 typedef struct {
   TokenType type;
   char* text;
-  int col;
-  int row;
 } Token ;
 
+typedef struct {
+  char* src;
+  int pos;
+} Lexer;
+
+Token next_token(Lexer* lexer) {
+  assert(lexer != NULL);
+
+  while (isspace((unsigned char)lexer->src[lexer->pos]))
+    lexer->pos++;
+
+  if (lexer->src[lexer->pos] == '\0')
+    return (Token){.type = TOK_EOF, .text = ""};
+
+  char c = lexer->src[lexer->pos];
+
+  // word -> keyword or NAME
+  if (isalpha((unsigned char)c) || c == '_') {
+    int start = lexer->pos;
+    while (isalnum((unsigned char)lexer->src[lexer->pos]) ||
+           lexer->src[lexer->pos] == '_')
+      lexer->pos++;
+    int len = lexer->pos - start;
+    char *buf = malloc(len + 1);
+    memcpy(buf, &lexer->src[start], len);
+    buf[len] = '\0';
+    if (strcmp(buf, "func") == 0) return (Token){TOK_FUNC, buf};
+    if (strcmp(buf, "int") == 0) return (Token){TOK_INT, buf};
+    if (strcmp(buf, "if") == 0) return (Token){TOK_IF, buf};
+    if (strcmp(buf, "for") == 0) return (Token){TOK_FOR, buf};
+    if (strcmp(buf, "break") == 0) return (Token){TOK_BREAK, buf};
+    if (strcmp(buf, "return") == 0) return (Token){TOK_RETURN, buf};
+    return (Token){TOK_NAME, buf};
+  }
+
+  // number
+  if (isdigit((unsigned char)c)) {
+    int start = lexer->pos;
+    while (isdigit((unsigned char)lexer->src[lexer->pos]))
+      lexer->pos++;
+    int len = lexer->pos - start;
+    char *buf = malloc(len + 1);
+    memcpy(buf, &lexer->src[start], len);
+    buf[len] = '\0';
+    return (Token){TOK_NUMBER, buf};
+  }
+
+  // == vs =
+  if (c == '=') {
+    if (lexer->src[lexer->pos + 1] == '=') {
+      lexer->pos += 2;
+      return (Token){TOK_EQ, "="}; 
+    }
+    lexer->pos++;
+    return (Token){TOK_ASSIGN, "="};
+  }
+
+  // single chars
+  lexer->pos++;
+  switch (c) {
+    case '+': return (Token){TOK_PLUS, "+"};
+    case '>': return (Token){TOK_GT, ">"};
+    case '(': return (Token){TOK_LPAREN, "("};
+    case ')': return (Token){TOK_RPAREN, ")"};
+    case '{': return (Token){TOK_LBRACE, "{"};
+    case '}': return (Token){TOK_RBRACE, "}"};
+    case ';': return (Token){TOK_SEMI, ";"};
+    case ',': return (Token){TOK_COMMA, ","};
+  }
+
+  return next_token(lexer);
+}
+
+const char *tok_name(TokenType t) {
+  switch (t) {
+    case TOK_FUNC: return "FUNC";
+    case TOK_INT: return "INT";
+    case TOK_NAME: return "NAME";
+    case TOK_NUMBER: return "NUMBER";
+    case TOK_ASSIGN: return "ASSIGN";
+    case TOK_LPAREN: return "LPAREN";
+    case TOK_RPAREN: return "RPAREN";
+    case TOK_SEMI: return "SEMI";
+    case TOK_EOF: return "EOF";
+    default: return "OTHER";
+  }
+}
+
 int main(){
-  printf("Hello world!\n"); 
+  char *src = "x = increment(x);";
+  Lexer lex = {src, 0};
+  for (;;) {
+    Token t = next_token(&lex);
+    printf("%s %s\n", tok_name(t.type), t.text);
+    if (t.type == TOK_EOF) break;
+  }
 }
